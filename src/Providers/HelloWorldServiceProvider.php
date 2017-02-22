92bbcca22fd4df5861cd6dc0e9da0eb49578072f
jensk007 <info@belladecor.de>
2017-02-22 23:56:03 +0100

    commit
    commit

---
+++ src/Providers/HelloWorldServiceProvider.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace HelloWorld\Providers;
+
+
+use Plenty\Plugin\ServiceProvider;
+
+class HelloWorldServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register the service provider.
+     */
+
+    public function register()
+    {
+
+    }
+}
+...
+
+        public function register()
+        {
+            $this->getApplication()->register(HelloWorldRouteServiceProvider::class);
+        }
+        ...
+
+            public function map(Router $router)
+            {
+                $router->get('hello','HelloWorld\Controllers\ContentController@sayHello');
+            }
